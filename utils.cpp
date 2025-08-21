a28b9dd8c5f49c8773464414719db415ec51e179
Michael Mouchous <michaelm11188@gmail.com>
2025-08-21 12:11:49 +0200

    triselect branch

--- utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "utils.h"
-
-QRectF halfRect(const QRectF &rect)
-{
-    auto w = rect.width() / 4;
-    auto h = rect.height() / 4;
-    return QRectF(rect.x() + w, rect.y() + h, 2 * w, 2 * h);
-}
--- utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <QRectF>
-QRectF halfRect(const QRectF &rect);
-#endif // UTILS_H
